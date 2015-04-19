e23faa7bf6192420b57e71672cf2ab49d56ebf9e
Favyen Bastani <fbastani@perennate.com>
2015-04-19 15:40:41 -0400

    Separate some of peerstore code to separate Peer object.

---
+++ src/fluidbackup/peer.go
@@ -0,0 +1,55 @@
+const (
+	STATUS_ONLINE = 0
+	STATUS_OFFLINE = 1
+)
+
+type PeerId struct {
+	Address string
+	Port int
+}
+
+type Peer struct {
+	mu sync.Mutex
+	id PeerId
+	status int
+	localBytes int // how many bytes we've agreed to store for this peer
+	remoteBytes int // how many bytes peer is willing to store for us
+
+	// cached values
+	localUsedBytes int
+	remoteUsedBytes int
+}
+
+func (this *Peer) proposeAgreement(localBytes int, remoteBytes int) bool {
+	if this.protocol.proposeAgreement(this.id, localBytes, remoteBytes) {
+		this.mu.Lock()
+		this.localBytes += localBytes
+		this.remoteBytes += remoteBytes
+		this.mu.Unlock()
+		return true
+	}
+
+	return false
+}
+
+/*
+ * Attempts to reserve a number of bytes for storage on this peer.
+ * Returns true if the bytes have been reserved for use by caller, or false if reservation failed.
+ */
+func (this *Peer) reserveBytes(bytes int) bool {
+	this.mu.Lock()
+	defer this.mu.Unlock()
+
+	if this.remoteBytes - this.remoteUsedBytes >= bytes {
+		this.remoteUsedBytes += bytes
+		return true
+	} else {
+		return false
+	}
+}
+
+func (this *Peer) isOnline() bool {
+	this.mu.Lock()
+	defer this.mu.Unlock()
+	return this.status == STATUS_ONLINE
+}
--- src/fluidbackup/peerstore.go
+++ src/fluidbackup/peerstore.go
@@ -1,47 +1,21 @@
 package fluidbackup
 
-type PeerAgreement struct {
-	// stable values
-	agreementId string
-	peer Peer // peer with whom we have this agreement
-	localBytes int // how many bytes we're storing for the peer
-	remoteBytes int // how many bytes the peer is storing for us
-
-	// cached values
-	localUsedBytes int
-	remoteUsedBytes int
-}
-
-type Peer struct {
-	Id string
-}
-
-const (
-	STATUS_ONLINE = 0
-	STATUS_OFFLINE = 1
-)
-
-type PeerStatus struct {
-	status int
-}
-
 type PeerRequest struct {
 	Bytes int
-	IgnorePeers map[Peer]bool
-	KnownPeers map[Peer]PeerStatus
+	IgnorePeers map[PeerId]bool
 }
 
 type PeerStore struct {
 	mu sync.Mutex
 	protocol *Protocol
-	peerAgreements map[Peer]*PeerAgreement
+	peers map[PeerId]*Peer
 	lastRequest *PeerRequest
 }
 
 func MakePeerStore(protocol *Protocol) *PeerStore {
 	this := new(PeerStore)
 	this.protocol = protocol
-	this.peerAgreements = make(map[Peer]*PeerAgreement)
+	this.peers = make(map[PeerId]*Peer)
 	this.lastRequest = nil
 
 	go func() {
@@ -58,22 +32,21 @@ func MakePeerStore(protocol *Protocol) *PeerStore {
  * Caller requires [bytes] free bytes to use and doesn't want the peer to be in [ignorePeers] list.
  * Returns nil if we aren't able to satisfy the request (in this case we should also launch task to get more space).
  */
-func (this *PeerStore) FindAvailablePeer(bytes int, ignorePeers map[Peer]bool) *Peer {
+func (this *PeerStore) FindAvailablePeer(bytes int, ignorePeers map[PeerId]bool) *Peer {
 	this.mu.Lock()
 	defer this.mu.Unlock()
 
-	for peer, agreement := range this.peerAgreements {
-		_, shouldIgnore := ignorePeers[peer]
-		if !shouldIgnore && agreement.remoteBytes - agreement.remoteUsedBytes >= bytes {
-			// this one works, has enough bytes and not in ignorePeers
-			agreement.remoteUsedBytes += bytes
-			return peer
+	for peerId, peer := range this.peers {
+		_, shouldIgnore := ignorePeers[peerId]
+		if !shouldIgnore && peer.reserveBytes(request.bytes) {
+			return peerId
 		}
 	}
 
-	// none of our existing agreements satisfied this request
-	// update last request so that we eventually satisfy
+	// none of our peers could satisfy this request with existing agreements
+	// update last request so that we will create new agreement and eventually satisfy
 	lastRequest = &PeerRequest{Bytes: bytes, IgnorePeers: ignorePeers}
+	return nil
 }
 
 func (this *PeerStore) update() {
@@ -84,7 +57,7 @@ func (this *PeerStore) update() {
 		request := *this.lastRequest
 		this.lastRequest = nil
 		this.mu.Unlock()
-		createAgreementSatisfying(request)
+		this.createAgreementSatisfying(request)
 		return
 	}
 
@@ -94,27 +67,15 @@ func (this *PeerStore) update() {
 func (this *PeerStore) createAgreementSatisfying(request PeerRequest) {
 	// pick random online peer that isn't in the ignore list
 	this.mu.Lock()
-	possiblePeers = make([]Peer)
-	for peer, status := range this.knownPeers {
-		_, shouldIgnore := request.ignorePeers[peer]
-		if !shouldIgnore && status.status == STATUS_ONLINE {
+	possiblePeers = make([]*Peer)
+	for peerId, peer := range this.peers {
+		_, shouldIgnore := request.ignorePeers[peerId]
+		if !shouldIgnore && peer.isOnline() {
 			possiblePeers = append(possiblePeers, peer)
 		}
 	}
 	randomPeer := possiblePeers[rand.Intn(len(possiblePeers))]
 	this.mu.Unlock() // don't want to hold lock during long-lived peer communication
 
-	if this.protocol.proposeAgreement(randomPeer, request.bytes, request.bytes) {
-		this.mu.Lock()
-		// update our agreement table to match the accepted agreement
-		_, existingAgreement = this.peerAgreements[randomPeer]
-
-		if existingAgreement {
-			existingAgreement.remoteBytes += request.bytes
-			existingAgreement.localBytes += request.bytes
-		} else {
-			this.peerAgreements[randomPeer] = PeerAgreement{agreementId: randSeq(16), peer: randomPeer, localBytes: request.bytes, remoteBytes: request.bytes}
-		}
-		this.mu.Unlock()
-	}
+	randomPeer.proposeAgreement(request.bytes, request.bytes)
 }
