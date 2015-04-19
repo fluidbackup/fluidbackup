f5ba8e160e7ea3f4898ce69185dfd0cdc0db46f2
Favyen Bastani <fbastani@perennate.com>
2015-04-19 16:54:28 -0400

    Update filestore.go to reflect name.

--- src/fluidbackup/filestore.go
+++ src/fluidbackup/filestore.go
@@ -4,14 +4,11 @@ import "fmt"
 
 /* Represents a data store provider. Is the entity that actually
 stores information. */
-type FluidServer struct {
-}
+type FileStore struct {
 
-func (fv *FluidServer) PrintHelloWorld() {
-	fmt.Println("Hellow World")
 }
 
 // A temporary store data file that is archaic
-func (fv *FluidServer) StoreData(data []byte) {
+func (this *FileStore) StoreData(path string, data []byte) {
 
 }
