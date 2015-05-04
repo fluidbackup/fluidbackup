98fafd1c348dc45372bd3edd47ba8e4b71dfa1e0
Favyen Bastani <fbastani@perennate.com>
2015-05-04 18:00:49 -0400

    Add test for the previous commit (reread files).

--- src/fluidbackup/basic_test.go
+++ src/fluidbackup/basic_test.go
@@ -114,3 +114,61 @@ func TestStable(t *testing.T) {
 		inst.Stop()
 	}
 }
+
+func TestReread(t *testing.T) {
+	fileContents := make([]byte, 1500 * 1024)
+	for i := range fileContents {
+		fileContents[i] = byte(rand.Int() % 256)
+	}
+	err := ioutil.WriteFile("orange.txt", fileContents, 0644)
+	if err != nil {
+		t.Error("failed to write random contents to orange.txt: " + err.Error())
+	}
+
+	mainPort := 19838
+	bMain := MakeFluidBackup(mainPort)
+	bMain.fileStore.RegisterFile("orange.txt")
+	time.Sleep(time.Second)
+	bMain.Save()
+	bMain.Stop()
+	time.Sleep(2 * time.Second)
+	bMain = MakeFluidBackup(mainPort)
+	bMain.Load()
+
+	bOther := make([]*FluidBackup, 11)
+	for i := range bOther {
+		port := 19839 + i
+		bOther[i] = MakeFluidBackup(port)
+
+		if i < 5 {
+			bMain.peerList.DiscoveredPeer(PeerId{Address: "127.0.0.1", Port: port})
+		} else {
+			bOther[i].peerList.DiscoveredPeer(PeerId{Address: "127.0.0.1", Port: mainPort});
+		}
+	}
+
+	time.Sleep(5 * time.Second)
+
+	err = os.Remove("orange.txt")
+
+	if err != nil {
+		t.Error("Encountered error while removing file: " + err.Error())
+		return
+	}
+
+	bMain.fileStore.RecoverFile("orange.txt")
+	newFileContents, err := ioutil.ReadFile("orange.txt")
+	if err != nil {
+		t.Error("failed to read from recovered orange.txt: " + err.Error())
+	}
+
+	if !bytes.Equal(fileContents, newFileContents) {
+		t.Error("recovered file does not match original")
+	}
+
+	// shut down fluidbackup instances
+	bMain.Stop()
+	for _, inst := range bOther {
+		inst.Stop()
+	}
+}
